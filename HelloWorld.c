02e47f284b965c4ea56064d8f1b50805ec1da18c
Isaac Lee <i_m_iz@hotmail.com>
2020-01-04 21:35:20 +0000

    Copy files

---
+++ HelloWorld.c
@@ -0,0 +1,33 @@
+#include <hello.h>
+
+void main() {
+	// int number_test = 12321;
+	// char character_test = 't';
+	// char *string_test = "testing";
+	// character = '2';
+	// printhelloworld();
+	// printf("Testing\n");
+	if(strcmp(returnstring(), "h") == 0) {
+		// helloworld();
+		// printhelloworld();
+		char *string_test = returnstring();
+		printf("%s\n", string_test);
+	}
+	// int so = sizeof(number_test);
+		// printf("Test print: %s\n", string_test);
+}
+// TokenHashIf
+// TokenContinue
+// TokenContinue
+// TokenEndOfLine
+// TokenVoidType
+// TokenLeftBrace
+// TokenIdentifier
+// TokenRightBrace
+// TokenEndOfLine
+// TokenIdentifier
+// TokenLeftBrace
+// TokenIdentifier
+// "Hello World"
+// TokenRightBrace
+// TokenEndOfLine
---
+++ hello.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h> 
+#include <string.h> 
+#include <socket.h>
+
+void printhelloworld() {
+  printf("Hello World\n");
+}
+
+char *returnstring() {
+	return "helloworld";
+}
