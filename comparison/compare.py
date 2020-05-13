f6ebe4a8a72f2ff2e4c1b93dec4c526d9022e8ed
Isaac Lee <i_m_iz@hotmail.com>
2020-05-13 10:54:00 +0100

    cleaned and commented compare.py

--- comparison/compare.py
+++ comparison/compare.py
@@ -1,27 +1,21 @@
-# import os
-# os.system('./picoc servers/server.c > report/')
-
-
 import subprocess
-import time
 import os
 import sys
-import ast
-import re
 import json
 from datetime import datetime
 from pathlib import Path
 
+# directory where the corpus is
 directory = "../corpus"
-# files = ["deadbits_bindshell.c"]
-# files = ["server_multiclient_array.c"]
+
+# file names of the corpus
 files = ["arturgontijo_directshell.c","bressan3_server.c","deadbits_bindshell.c","dmknght_linuxbind.c",
 "fakhrizulkifli_n00bshell.c","geyslan_shellbindtcp.c","m0nad_bindshell.c",
 "tutorial_server.c","tutorial_server_multiclient.c","tutorial_server_multiclient_array.c","tutorial_server2.c",
 "utsav3_server.c","valbrux_bindshell.c"]
+
+# output labels
 labels = ["===JSON OUTPUT===","===END==="]
-# labels= ["===ALL SOCKETS===","===MAY BE LISTENING SOCKETS===","===FORK ON LINE===","===END==="]
-output = ""
 
 totalTimePicoc = 0
 totalTimeRegex = 0
@@ -30,9 +24,11 @@ Path("../reports").mkdir(parents=True, exist_ok=True)
 
 for file in files:
 	orig_stdout = sys.stdout
+
 	count = 1
 	outputDir = ""
 
+	# create file's report directory
 	while(count):
 		outputDir = "../reports/"+datetime.today().strftime('%Y%m%d')+"_"+str(count)+"_"+file+"/"
 		
@@ -42,21 +38,19 @@ for file in files:
 		else:
 			count += 1
 
-
-	# start_time = time.time()
+	# execute picoc and syntax-based search program with file
 	spPicoc = subprocess.Popen(["../picoc", directory+"/"+file], 
 	           stdout=subprocess.PIPE, 
 	           stderr=subprocess.STDOUT)
-	# print("===GENERATED IN %s SECONDS===" % (time.time() - start_time))
-	# start_time = time.time()
+
 	spRegex = subprocess.Popen(["python3.7", "regex.py", directory+"/"+file], 
 	           stdout=subprocess.PIPE, 
 	           stderr=subprocess.STDOUT)
-	# print("===GENERATED IN %s SECONDS===" % (time.time() - start_time))
 	
 	stdoutPicoc,stderrPicoc = spPicoc.communicate()
 	stdoutRegex,stderrRegex = spRegex.communicate()
 
+	# write outputs to respective files
 	with open(outputDir+"picoc", 'wb') as myfile:
 		myfile.write(stdoutPicoc)
 		myfile.close()
@@ -65,21 +59,19 @@ for file in files:
 		myfile.write(stdoutRegex)
 		myfile.close()
 
-	# with open(outputDir+"diff", "a") as f:
+	# generate file for output difference
 	with open(outputDir+"diff", "a") as f:
+		# change stdout to f, print() will write to file
 		sys.stdout = f
+
 		overallDiff = 0
 		diff = 0
 
 		print("*****COMPARING "+file+"*****")
-
-		# outputPicoc = list(map(lambda x: x.decode("utf-8"), stdoutPicoc.splitlines()))
-		# outputRegex = list(map(lambda x: x.decode("utf-8"), stdoutRegex.splitlines()))
 		decodedPicoc = stdoutPicoc.decode("utf-8")
 		decodedRegex = stdoutRegex.decode("utf-8")
 
-		# print(decodedRegex)
-
+		# obtain index for start and end of JSON portion in output
 		startIndexPicoc = decodedPicoc.index(labels[0]) + len(labels[0])
 		endIndexPicoc = decodedPicoc.index(labels[1])
 		startIndexRegex = decodedRegex.index(labels[0]) + len(labels[0])
@@ -88,12 +80,15 @@ for file in files:
 		jsonPicoc = json.loads(stdoutPicoc[startIndexPicoc:endIndexPicoc])
 		jsonRegex = json.loads(stdoutRegex[startIndexRegex:endIndexRegex])
 
-
 		for (k,v), (k2,v2) in zip(jsonPicoc.items(), jsonRegex.items()):
+			# not comparing var identifier, func identifier and analysis time
+			# because syntax-based search program does not have var_id and func_id
 			if k == "var_id" or k == "func_id" or k == "analysis_time":
 				continue
 
 			print("===%s===" % k.upper().replace("_"," "))
+
+			# compare sockets
 			if k == "all_sockets" or k == "maybelistening_sockets" :
 				if k == "maybelistening_sockets":
 					v = [s for s in v if s["state"] == "MayBeListening"]
@@ -131,6 +126,7 @@ for file in files:
 				print()
 				diff = 0
 
+			# compare fork and exec
 			else:
 				v = set(v)
 				v2 = set(v2)
@@ -157,91 +153,16 @@ for file in files:
 
 				print()
 
-		# for key in jsonRegex:
-		# 	print(key)
-		# 	if key == "all_sockets" or key == "maybelistening_sockets":
-		# 		for socket in jsonRegex[key]:
-		# 			print(socket)
-			# jsonRegex[key]
-		# print(jsonPicoc["all_sockets"])
-
-		# for i in range(len(labels)):
-		# 	if i+1 == len(labels):
-		# 		break
-
-			# print(labels[i])
-			# start = stdoutRegex.decode("utf-8").index(labels[i])
-			# end = stdoutRegex.decode("utf-8").index(labels[i+1])
-			# print(json.loads(stdoutRegex[start+len(labels[i]):end]))
-			# x = stdoutRegex.decode("utf-8").replace("\n","").replace("\r","")
-			# print(x)
-			# print(stdoutPicoc.decode("utf-8").replace("\n",""))
-			# print(json.loads(x))
-			# x = json.loads()
-			# print(x)
-
-			# if i+1 != len(labels):
-			# 	if(i == 0): # ===ALL SOCKETS===
-			# 		picocSet = set()
-			# 		for x in outputPicoc[outputPicoc.index(labels[i])+1:outputPicoc.index(labels[i+1])]:
-			# 			dupPortionStr = x.split(":")[3]
-			# 			dupPortionList = ast.literal_eval(dupPortionStr)
-			# 			dupPortionList.sort()
-			# 			picocSet.add(x.replace(dupPortionStr, str(dupPortionList)))
-
-			# 		regexSet = set()
-			# 		for x in outputRegex[outputRegex.index(labels[i])+1:outputRegex.index(labels[i+1])]:
-			# 			dupPortionStr = x.split(":")[3]
-			# 			dupPortionList = ast.literal_eval(dupPortionStr)
-			# 			dupPortionList.sort()
-			# 			regexSet.add(x.replace(dupPortionStr, str(dupPortionList)))
-			# 	else:
-			# 		picocSet = set(outputPicoc[outputPicoc.index(labels[i])+1:outputPicoc.index(labels[i+1])])
-			# 		regexSet = set(outputRegex[outputRegex.index(labels[i])+1:outputRegex.index(labels[i+1])])
-
-			# 	print("\t---Difference Picoc\\Regex---")
-			# 	setDiff = picocSet.difference(regexSet)
-			# 	if not setDiff:
-			# 		print("\t\tNIL")
-			# 	else:
-			# 		diff = 1
-
-			# 	for x in picocSet.difference(regexSet):
-			# 		print("\t\t"+x)
-
-			# 	print()
-
-			# 	print("\t---Difference Regex\\Picoc---")
-			# 	setDiff = regexSet.difference(picocSet)
-			# 	if not setDiff:
-			# 		print("\t\tNIL")
-			# 	else:
-			# 		diff = 1
-
-			# 	for x in regexSet.difference(picocSet):
-			# 		print("\t\t"+x)
-				
-			# print()
-
 		f.close()
 
 
 	sys.stdout = orig_stdout
-
-	# matches = re.finditer(r"===GENERATED IN (.*) SECOND\(S\)===", stdoutPicoc.decode("utf-8"))
-	# for match in matches:
-	# 	time = float(match[1])
-	# 	totalTimePicoc += time
-
-	# matches = re.finditer(r"===GENERATED IN (.*) SECOND\(S\)===", stdoutRegex.decode("utf-8"))
-	# for match in matches:
-	# 	time = float(match[1])
-	# 	totalTimeRegex += time
 	
 	totalTimePicoc += jsonPicoc["analysis_time"]
 	totalTimeRegex += jsonRegex["analysis_time"]
 
 	if overallDiff:
 		print("Diff in %s" % file)
+		
 print("Average time taken by PicoC: %.5f" % (totalTimePicoc))
 print("Average time taken by Regex: %.5f" % (totalTimeRegex))
