dc13ad1c3b3a74c1a8afed780eea8a902920da44
Isaac Lee <i_m_iz@hotmail.com>
2020-05-13 10:59:48 +0100

    cleaned and commented compare_groundtruth.py

--- comparison/compare_groundtruth.py
+++ comparison/compare_groundtruth.py
@@ -1,34 +1,37 @@
 import subprocess
-import time
-import os
-import sys
-import ast
-import re
 import json
 from datetime import datetime
 from pathlib import Path
 
+# directory where the corpus is
 directory = "../corpus"
+
+# file names of the corpus
 files = ["arturgontijo_directshell.c","bressan3_server.c","deadbits_bindshell.c","dmknght_linuxbind.c",
 "fakhrizulkifli_n00bshell.c","geyslan_shellbindtcp.c","m0nad_bindshell.c",
 "tutorial_server.c","tutorial_server_multiclient.c","tutorial_server_multiclient_array.c","tutorial_server2.c",
 "utsav3_server.c","valbrux_bindshell.c"]
+
+# output labels
 labels = ["===JSON OUTPUT===","===END==="]
 
 for file in files:
+	# execute picoc with file
 	spPicoc = subprocess.Popen(["../picoc", directory+"/"+file], 
 	           stdout=subprocess.PIPE, 
 	           stderr=subprocess.STDOUT)
 	
 	stdoutPicoc,stderrPicoc = spPicoc.communicate()
+
 	decodedPicoc = stdoutPicoc.decode("utf-8")
 
+	# obtain index for start and end of JSON portion in output
 	startIndexPicoc = decodedPicoc.index(labels[0]) + len(labels[0])
 	endIndexPicoc = decodedPicoc.index(labels[1])
 
 	jsonPicoc = json.loads(stdoutPicoc[startIndexPicoc:endIndexPicoc])
 
-
+	# load groundtruth JSON string
 	with open(directory+"/groundtruth/"+file[:-2], "r") as f:
 		fileString = f.read()
 		startIndex = fileString.index(labels[0]) + len(labels[0])
@@ -41,9 +44,11 @@ for file in files:
 	diff = 0
 
 	for (k,v), (k2,v2) in zip(jsonPicoc.items(), jsonGroundtruth.items()):
-		if k == "analysis_time": #k == "var_id" or k == "func_id" or
+		# not comparing analysis time
+		if k == "analysis_time":
 			continue
 
+		# compare sockets
 		if k == "all_sockets" or k == "maybelistening_sockets" :
 			if k == "maybelistening_sockets":
 				v = [s for s in v if s["state"] == "MayBeListening"]
@@ -65,6 +70,7 @@ for file in files:
 					diff = 1
 					break
 
+		# compare fork, exec, var_id, func_id
 		else:
 			v = set(v)
 			v2 = set(v2)
